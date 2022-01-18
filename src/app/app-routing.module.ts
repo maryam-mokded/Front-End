51003c2546d7e7bbd9577e916ac6c3256671d02a
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2022-01-18 21:43:47 +0100

    Project Structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes , CanActivate} from '@angular/router';
+
+const routes :Routes =[
+
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [
+  ]
+})
+export class AppRoutingModule { }
