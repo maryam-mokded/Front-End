dd899cb245c2dda81f3e61c9f2ee12b6a850349d
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2022-01-26 14:51:33 +0100

    Update about.component.ts
    About Page Template Ts

--- src/app/external-components/about/about.component.ts
+++ src/app/external-components/about/about.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  counter: number = 0;
+  ShowImgCim: boolean = false;
+  ShowImgLoc: boolean = false;
+  ShowDescription: boolean = false;
+  intervalId?: any;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.counter = 0;
+    this.ShowImgCim = true;
+    this.ShowImgLoc = false;
+    this.ShowDescription = false;
+    this.ShowElements();
   }
 
+  ShowElements() {
+    this.intervalId = setInterval(() => {
+      this.counter = this.counter + 1;
+      console.log(this.counter);
+      if (this.counter == 1) {
+        this.ShowDescription = false;
+        this.ShowImgCim = true;
+        this.ShowImgLoc = false;
+      } else if (this.counter == 6) {
+        this.ShowDescription = true;
+        this.ShowImgCim = false;
+        this.ShowImgLoc = false;
+      } else if (this.counter == 12) {
+        this.ShowDescription = false;
+        this.ShowImgCim = false;
+        this.ShowImgLoc = true;
+      }
+      if (this.counter === 18) {
+         this.counter = 0;
+        // clearInterval(this.intervalId);
+     }
+    }, 1000);
+
+  }
 }
