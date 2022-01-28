5d741e3ba919e20ed858ccf768bea81fea3a7e04
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2022-01-28 11:03:10 +0100

    Modification Page Home and Page Login

--- src/app/external-components/home/home.component.ts
+++ src/app/external-components/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit ,HostListener } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  Image1:number=1;
   LienCard:String="";
   idImage:String="slide-dot-1";
   counter: number = 0;
@@ -35,17 +36,17 @@ export class HomeComponent implements OnInit {
   AfficheImage1(){
     this.SrcImage = "../../../assets/ImageAccueil/jobOffer.png";
     this.idImage ="slide-dot-1"
-    this.LienCard = "Tender notice N°04/2022 “Acquisition of a carbon and sulfur analyzer”"
+    this.LienCard = "Image1"
   }
   AfficheImage2(){
     this.SrcImage ="../../../assets/ImageAccueil/resultat.gif";
     this.idImage ="slide-dot-2"
-    this.LienCard = "Preliminary list of candidates (Masters) selected after the oral and psychotechnical examination of the recruitment competition for the year 2019"
+    this.LienCard = "Image2"
   }
   AfficheImage3(){
     this.SrcImage ="../../../assets/ImageAccueil/press.png";
     this.idImage ="slide-dot-3"
-    this.LienCard = "Press release of 28/12/2010"
+    this.LienCard = "Image3"
   }
 
   ChangerImageCard() {
@@ -55,16 +56,15 @@ export class HomeComponent implements OnInit {
       if (this.counter == 1) {
        this.SrcImage = "../../../assets/ImageAccueil/jobOffer.png";
        this.idImage ="slide-dot-1"
-       this.LienCard = "Tender notice N°04/2022 “Acquisition of a carbon and sulfur analyzer”"
-
+       this.LienCard = "Image1"
       } else if (this.counter == 6) {
         this.SrcImage ="../../../assets/ImageAccueil/resultat.gif";
         this.idImage ="slide-dot-2"
-        this.LienCard = "Preliminary list of candidates (Masters) selected after the oral and psychotechnical examination of the recruitment competition for the year 2019"
+        this.LienCard = "Image2"
       } else if (this.counter == 12) {
         this.SrcImage ="../../../assets/ImageAccueil/press.png";
         this.idImage ="slide-dot-3"
-        this.LienCard = "Press release of 28/12/2010"
+        this.LienCard = "Image3"
       }
       if (this.counter === 18) {
          this.counter = 0;
