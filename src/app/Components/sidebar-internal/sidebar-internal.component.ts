d858817d90a0712a3c8e891e3e835356557d58b9
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2022-02-06 14:25:28 +0100

    Modifier Profil

--- src/app/Components/sidebar-internal/sidebar-internal.component.ts
+++ src/app/Components/sidebar-internal/sidebar-internal.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from 'src/app/Models/user';
+import { UserService } from 'src/app/Services/user.service';
+
+
 @Component({
   selector: 'app-sidebar-internal',
   templateUrl: './sidebar-internal.component.html',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarInternalComponent implements OnInit {
 
-  constructor() { }
+  user!:User;
+  constructor(
+    private userServ:UserService,
+  ) { }
 
   ngOnInit(): void {
+    this.DetailsUser();
   }
 
+
+
+  DetailsUser(){
+    //id de l'utilisateur connecter
+    this.userServ.ConsulterEmployee(2).subscribe((u) => {
+      this.user =u;
+      console.log(this.user);
+    });
+  }
 }
--- src/app/internal-components/profile/profile.component.ts
+++ src/app/internal-components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Models/user';
+import { UserService } from 'src/app/Services/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,19 +9,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+
   test: boolean = true;
   EtatProfil:String="Profile"
-  constructor() {}
 
-  ngOnInit(): void {}
+  myForm!: FormGroup;
+  user!:User;
 
-  modifierProfil() {
+  constructor(
+    private userServ:UserService,
+  ) {}
+
+  ngOnInit(): void {
+    this.DetailsUser();
+    this.ValidatedForm();
+  }
+
+  ValidatedForm(){
+    this.myForm = new FormGroup({
+      'nom' : new FormControl(null,[Validators.required, Validators.minLength(2),Validators.maxLength(30)]),
+      'prenom' : new FormControl(null,[Validators.required, Validators.minLength(2),Validators.maxLength(30)]),
+      'profession' : new FormControl(null,[Validators.required, Validators.minLength(2),Validators.maxLength(30)]),
+      'matricule' : new FormControl(null,[Validators.required, Validators.minLength(5),Validators.maxLength(5)]),
+      'adresse' : new FormControl(null,[Validators.required, Validators.minLength(2),Validators.maxLength(30)]),
+      'cin' : new FormControl(null,[Validators.required , Validators.pattern("[0-9]{8}")]),
+      'tel' : new FormControl(null,[Validators.required,Validators.pattern("[0-9]{8}")]),
+      'email' : new FormControl(null, [Validators.required,Validators.email]),
+      'dateEmbauche' : new FormControl(null, [Validators.required]),
+      'idDirection' : new FormControl(null),
+
+    });
+  }
+  get direction(){
+    return this.myForm.get('idDirection') ;
+  }
+  get adresse(){
+    return this.myForm.get('adresse') ;
+  }
+
+  get dateEmbauche(){
+    return this.myForm.get('dateEmbauche') ;
+  }
+
+  get name(){
+    return this.myForm.get('nom') ;
+  }
+  get matricule(){
+    return this.myForm.get('matricule') ;
+  }
+  get prenom(){
+    return this.myForm.get('prenom') ;
+  }
+  get email(){
+    return this.myForm.get('email') ;
+  }
+
+  get cin(){
+    return this.myForm.get('cin') ;
+  }
+
+  get profession(){
+    return this.myForm.get('profession') ;
+  }
+
+  get tel(){
+    return this.myForm.get('tel') ;
+  }
+
+
+
+  DetailsUser(){
+    //id de l'utilisateur connecter
+    this.userServ.ConsulterEmployee(2).subscribe((u) => {
+      this.user =u;
+      console.log(this.user);
+    });
+  }
+
+
+  modifierProfil(user:User) {
     if (this.test == true) {
       this.test = false;
       this.EtatProfil = "Editer"
     } else {
-      this.test = true;
-      this.EtatProfil = "Profile"
+      this.userServ.modifierUser(user.direction?.id_Direction,user.id_User,user).subscribe(u =>{
+        console.log("User Ajouter avec succees !")
+        window.location.reload()
+      })
     }
   }
 }
